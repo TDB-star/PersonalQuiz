c2feda8fd4f887163f889584cdd9fea725f4bb16
TDB-star <tatiana.blache@gmail.com>
2021-07-20 17:32:13 +0200

    Initial Commit

---
+++ PersonalQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuiz
+//
+//  Created by Tatiana Dmitrieva on 20/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
